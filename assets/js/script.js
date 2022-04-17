75433eec07b5e5e3a128290436d609018ceba6cf
Pilarac <pilaralvarezcastro2@gmail.com>
2022-04-16 20:18:27 -0400

    agregar link github

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(function () {
+  
+     
+    
+    $('#enviar_correo').click(function (){
+      alert("El correo fue enviado")
+    });
+  
+    $('h4').dblclick(function (){
+      $("h4").css("color", "blue");
+    })
+    
+  });
